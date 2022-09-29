928367dfed282d35ce0ea63ed98702db43d2dc6d
prem-cashfree <prem.chand@cashfree.com>
2022-09-29 12:59:59 +0530

    Add idempotency feature for accepting same request while order creation

--- modules/gateways/callback/cashfree.php
+++ modules/gateways/callback/cashfree.php
@@ -24,44 +24,73 @@ if (!$gatewayParams['type']) {
     die("Module Not Activated");
 }
 
-$appId              = $gatewayParams["appId"];
-$secretKey          = $gatewayParams["secretKey"];
+$appId      = $gatewayParams["appId"];
+$secretKey  = $gatewayParams["secretKey"];
 
 //Gateway response parameters
-$cashfreeOrderId    = $_POST["orderId"];
+$cashfreeOrderId    = $_REQUEST["order_id"];
 $invoiceId          = substr($cashfreeOrderId, strpos($cashfreeOrderId, "_") + 1);
-$transactionId      = $_POST['referenceId'];
-$paymentAmount      = $_POST["orderAmount"];
 
 // Validate Callback Invoice ID.
-$invoiceId          = checkCbInvoiceID($invoiceId, $gatewayParams['name']);
+$invoiceId  = checkCbInvoiceID($invoiceId, $gatewayParams['name']);
+$error      = "";
 
-$error = "";
+try {
+    $apiEndpoint = ($gatewayParams["testMode"] == 'on') ? 'https://sandbox.cashfree.com/pg/orders' : 'https://api.cashfree.com/pg/orders'; 
+    $getPaymentUrl = $apiEndpoint."/".$cashfreeOrderId."/payments";
 
-//Check if payment successfully paid
-if($_POST['txStatus'] == 'SUCCESS')
-{
-    // Check Callback Transaction ID.
-    checkCbTransID($transactionId);
-    try {
-        $data = "{$_POST['orderId']}{$_POST['orderAmount']}{$_POST['referenceId']}{$_POST['txStatus']}{$_POST['paymentMode']}{$_POST['txMsg']}{$_POST['txTime']}";
-        $hash_hmac = hash_hmac('sha256', $data, $secretKey, true) ;
-        $computedSignature = base64_encode($hash_hmac);
-        if ($_POST["signature"] != $computedSignature) {
-            $success = false;
-            $error = 'CASHFREE_ERROR:Invalid Signature';
-        } else {
-            $success = true;
-        }
-    } catch (Exception $e) {
-        $success = false;
-        $error ="WHMCS_ERROR:Request to Cashfree Failed";
-    }
-}
-else {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL             => $getPaymentUrl,
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_ENCODING        => "",
+        CURLOPT_MAXREDIRS       => 10,
+        CURLOPT_TIMEOUT         => 30,
+        CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST   => "GET",
+        CURLOPT_HTTPHEADER      => [
+            "Accept:            application/json",
+            "Content-Type:      application/json",
+            "x-api-version:     2021-05-21",
+            "x-client-id:       ".$appId,
+            "x-client-secret:   ".$secretKey
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+
+    $cfOrder = json_decode($response);
+} catch (Exception $e) {
     $success = false;
-    $error = $_POST['txMsg'];
+    $error ="WHMCS_ERROR:Request to Cashfree Failed";
 }
+
+if (null !== $cfOrder && !empty($cfOrder[0]->payment_status))
+{
+    if($cfOrder[0]->payment_status == 'SUCCESS')
+    {
+        $transactionId = $cfOrder[0]->cf_payment_id;
+        $paymentAmount = $cfOrder[0]->order_amount;
+        $success = true;
+    }
+    else {
+        $success = false;
+        $error = $cfOrder[0]->payment_message;
+    }
+
+} else {
+    if(!empty($cfOrder->message)) {
+        $error = $cfOrder->message;
+    } else {
+        $error = "Unable to process your order. Please contact support.";
+    }
+    $success = false;
+
+}
+//Check if payment successfully paid
 
 if ($success === true)
 {
@@ -69,14 +98,14 @@ if ($success === true)
     addInvoicePayment($invoiceId, $transactionId, $paymentAmount, 0, $gatewayParams["name"]);
     # Successful
     # Save to Gateway Log: name, data array, status
-    logTransaction($gatewayParams["name"], $_POST, "Successful");
+    logTransaction($gatewayParams["name"], $cfOrder[0], "Successful");
 
     header("Location: ".$gatewayParams['systemurl']."viewinvoice.php?id=" . $invoiceId."&paymentsuccess=true");
 }
 else 
 {
     # Save to Gateway Log: name, data array, status
-    logTransaction($gatewayParams["name"], $_POST, "Unsuccessful-".$error . ". Please check cashfree dashboard for order id: ".$cashfreeOrderId);
+    logTransaction($gatewayParams["name"], $_REQUEST["order_id"], "Unsuccessful-".$error . ". Please check cashfree dashboard for order id: ".$cashfreeOrderId);
 
     header("Location: ".$gatewayParams['systemurl']."viewinvoice.php?id=" . $invoiceId."&paymentfailed=true");
 }
