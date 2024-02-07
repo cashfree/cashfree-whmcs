285742b917447f1af582077728e863938b3ae5b8
prem-cashfree <prem.chand@cashfree.com>
2024-02-07 15:15:10 +0530

    Remove sentry

--- modules/gateways/cashfree.php
+++ modules/gateways/cashfree.php
@@ -1,9 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once __DIR__ . '/cashfree/sentry-php/vendor/autoload.php';
-
 define('CASHFREE_PLUGIN_VERSION', '2.2.0', true);
+define('API_VERSION', '2022-09-01');
 
 /**
  * WHMCS Cashfree Payment Gateway Module
@@ -83,12 +80,10 @@ function cashfree_link($params)
     // System Parameters
     $system_url      = $params['systemurl'];
     $module_name     = $params['paymentmethod'];
-    $invoice_details = WHMCS\Database\Capsule::table('tblinvoices')
-        ->where('id', $invoice_id)
-        ->first();
+    $invoice_details = mysql_fetch_assoc(select_query('tblinvoices', '*', array("id" => $invoice_id)));
 
     #check whether order is already paid or not, if paid then redirect to complete page
-    if($invoice_details->status === 'Paid')
+    if($invoice_details['status'] === 'Paid')
     {
         header("Location: ".$system_url."/viewinvoice.php?id=" . $invoice_id);
         
@@ -138,7 +133,7 @@ function generatePaymentSession($cf_request, $params)
     
     $get_order = getCfOrder($params, $get_cashfree_order_url);
 
-    if ($get_order && $get_order->order_status == "ACTIVE" &&
+    if ($get_order && $get_order->order_status == 'ACTIVE' &&
         $get_order->order_amount == $params['amount'] && $get_order->order_currency == $params['currency']) {
             return $get_order->payment_session_id;
     }
@@ -153,117 +148,96 @@ function generatePaymentSession($cf_request, $params)
 }
 
 function getCfOrder($params, $curl_url) {
-    try {
-        $curl = curl_init();
+    $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL             => $curl_url,
-            CURLOPT_RETURNTRANSFER  => true,
-            CURLOPT_ENCODING        => "",
-            CURLOPT_MAXREDIRS       => 10,
-            CURLOPT_TIMEOUT         => 30,
-            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST   => "GET",
-            CURLOPT_HTTPHEADER      => [
-                "Accept:            application/json",
-                "Content-Type:      application/json",
-                "x-api-version:     2022-09-01",
-                "x-client-id:       ".$params['appId'],
-                "x-client-secret:   ".$params['secretKey']
-            ],
-        ]);
+    curl_setopt_array($curl, [
+        CURLOPT_URL             => $curl_url,
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_ENCODING        => "",
+        CURLOPT_MAXREDIRS       => 10,
+        CURLOPT_TIMEOUT         => 30,
+        CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST   => "GET",
+        CURLOPT_HTTPHEADER      => [
+            "Accept:            application/json",
+            "Content-Type:      application/json",
+            "x-api-version:     " . API_VERSION,
+            "x-client-id:       ".$params['appId'],
+            "x-client-secret:   ".$params['secretKey']
+        ],
+    ]);
 
-        $response = curl_exec($curl);
+    $response = curl_exec($curl);
 
-        if ($response === false) {
-            throw new Exception('cURL Error: ' . curl_error($curl));
-        }
+    $err = curl_error($curl);
 
-        curl_close($curl);
-        
-        $cf_order = json_decode($response);
+    curl_close($curl);
 
-        if ($cf_order === null) {
-            throw new Exception("Failed to decode JSON response");
-        }
-        return $cf_order;
-    } catch (Exception $e) {
-        \Sentry\init([
-            'dsn' => 'https://d8571a24c9b11025a1471d5e6aee55ce@o330525.ingest.sentry.io/4506552108187648',
-            'release' => CASHFREE_PLUGIN_VERSION . '@whmcs',
-            'environment' => $cf_request['mode'],
-        ]);
-        \Sentry\captureException($e);
+    if ($err) {
+        die("Unable to create your order. Please contact support.");
     }
     
+    return json_decode($response);
 }
 
 function createCashfreeOrder($cf_request, $params, $api_endpoint) {
-    try {
-        $customer_details = array(
-            "customer_id"       => "WhmcsCustomer",
-            "customer_email"    => $params['clientdetails']['email'],
-            "customer_name"     => $params['clientdetails']['firstname'].' '.$params['clientdetails']['lastname'],
-            "customer_phone"    => $params['clientdetails']['phonenumber']
-        );
-        $order_meta = array(
-            "return_url"        => $cf_request['returnUrl'],
-            "notify_url"        => $cf_request['notifyUrl']
-        );
-        $request = array(
-            "customer_details"  => $customer_details,
-            "order_id"          => $cf_request['orderId'],
-            "order_amount"      => $params['amount'],
-            "order_currency"    => $params['currency'],
-            "order_note"        => "WHMCS Order",
-            "order_meta"        => $order_meta
-        );
+    $customer_details = array(
+        "customer_id"       => "WhmcsCustomer",
+        "customer_email"    => $params['clientdetails']['email'],
+        "customer_name"     => $params['clientdetails']['firstname'].' '.$params['clientdetails']['lastname'],
+        "customer_phone"    => $params['clientdetails']['phonenumber']
+    );
+    $order_meta = array(
+        "return_url"        => $cf_request['returnUrl'],
+        "notify_url"        => $cf_request['notifyUrl']
+    );
+    $request = array(
+        "customer_details"  => $customer_details,
+        "order_id"          => $cf_request['orderId'],
+        "order_amount"      => $params['amount'],
+        "order_currency"    => $params['currency'],
+        "order_note"        => "WHMCS Order",
+        "order_meta"        => $order_meta
+    );
 
-        $curl_postfield = json_encode($request);
+    $curl_postfield = json_encode($request);
 
-        $curl = curl_init();
+    $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL             => $api_endpoint,
-            CURLOPT_RETURNTRANSFER  => true,
-            CURLOPT_ENCODING        => "",
-            CURLOPT_MAXREDIRS       => 10,
-            CURLOPT_TIMEOUT         => 30,
-            CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST   => "POST",
-            CURLOPT_POSTFIELDS      => $curl_postfield,
-            CURLOPT_HTTPHEADER      => [
-                "Accept:            application/json",
-                "Content-Type:      application/json",
-                "x-api-version:     2022-09-01",
-                "x-client-id:       ".$params['appId'],
-                "x-client-secret:   ".$params['secretKey']
-            ],
-        ]);
+    curl_setopt_array($curl, [
+        CURLOPT_URL             => $api_endpoint,
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_ENCODING        => "",
+        CURLOPT_MAXREDIRS       => 10,
+        CURLOPT_TIMEOUT         => 30,
+        CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST   => "POST",
+        CURLOPT_POSTFIELDS      => $curl_postfield,
+        CURLOPT_HTTPHEADER      => [
+            "Accept:            application/json",
+            "Content-Type:      application/json",
+            "x-api-version:     " . API_VERSION,
+            "x-client-id:       ".$params['appId'],
+            "x-client-secret:   ".$params['secretKey']
+        ],
+    ]);
 
-        $response = curl_exec($curl);
+    $response = curl_exec($curl);
 
-        if ($response === false) {
-            throw new Exception('cURL Error: ' . curl_error($curl));
-        }
+    $err = curl_error($curl);
 
-        curl_close($curl);
-        
-        $cf_order = json_decode($response);
+    curl_close($curl);
 
-        if ($cf_order && !empty($cf_order->payment_session_id)) {
-            return $cf_order->payment_session_id;
-        } else {
-            return null;
-        }
-    } catch (Exception $e) {
+    if ($err) {
+        die("Unable to create your order. Please contact support.");
+    }
+    
+    $cf_order = json_decode($response);
 
-        \Sentry\init([
-            'dsn' => 'https://d8571a24c9b11025a1471d5e6aee55ce@o330525.ingest.sentry.io/4506552108187648',
-            'release' => CASHFREE_PLUGIN_VERSION . '@whmcs',
-            'environment' => $cf_request['mode'],
-        ]);
-        Sentry\captureException($e);
+    if ($cf_order && !empty($cf_order->payment_session_id)) {
+        return $cf_order->payment_session_id;
+    } else {
+        return null;
     }
 
 }
--- modules/gateways/cashfree/cashfree.php
+++ modules/gateways/cashfree/cashfree.php
@@ -12,14 +12,16 @@ require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/functions.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
-require_once __DIR__ . '/sentry-php/vendor/autoload.php';
 
-use WHMCS\Database\Capsule;
+define('API_VERSION', '2022-09-01');
+
 function checkTransIdExist($transaction_id) {
-    $transaction_details = Capsule::table('tblaccounts')
-        ->where('transid', $transaction_id)
-        ->first();
-    return ($transaction_details) ? true : false;
+    $result = select_query("tblaccounts", "id", array("transid" => $transaction_id));
+    $numRows = mysql_num_rows($result);
+    if ($numRows) {
+        return true;
+    }
+    return false;
 }
 
 // Detect module name from filename.
@@ -34,11 +36,11 @@ if (!$gateway_params['type']) {
     die("Module Not Activated");
 }
 
-$app_id      = $gateway_params["appId"];
-$secret_key  = $gateway_params["secretKey"];
+$app_id      = $gateway_params['appId'];
+$secret_key  = $gateway_params['secretKey'];
 
 //Gateway response parameters
-$cashfree_order_id    = $_REQUEST["order_id"];
+$cashfree_order_id    = $_REQUEST['order_id'];
 $invoice_id          = substr($cashfree_order_id, strpos($cashfree_order_id, "_") + 1);
 
 // Validate Callback Invoice ID.
@@ -46,77 +48,62 @@ $invoice_id = checkCbInvoiceID($invoice_id, $gateway_params['name']);
 
 $error = "";
 
-try {
-    $api_endpoint = ($gateway_params["testMode"] == 'on') ? 'https://sandbox.cashfree.com/pg/orders' : 'https://api.cashfree.com/pg/orders'; 
-    
-    $get_payment_url = $api_endpoint . "/" . $cashfree_order_id . "/payments";
 
-    $curl = curl_init();
+$api_endpoint = ($gateway_params['testMode'] == 'on') ? 'https://sandbox.cashfree.com/pg/orders' : 'https://api.cashfree.com/pg/orders'; 
 
-    curl_setopt_array($curl, [
-        CURLOPT_URL             => $get_payment_url,
-        CURLOPT_RETURNTRANSFER  => true,
-        CURLOPT_ENCODING        => "",
-        CURLOPT_MAXREDIRS       => 10,
-        CURLOPT_TIMEOUT         => 30,
-        CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST   => "GET",
-        CURLOPT_HTTPHEADER      => [
-            "Accept:            application/json",
-            "Content-Type:      application/json",
-            "x-api-version:     2022-09-01",
-            "x-client-id:       " . $app_id,
-            "x-client-secret:   " . $secret_key
-        ],
-    ]);
+$get_payment_url = $api_endpoint . "/" . $cashfree_order_id . "/payments";
 
-    $response = curl_exec($curl);
-    if ($response === false) {
-        throw new Exception('cURL Error: ' . curl_error($curl));
-    }
+$curl = curl_init();
 
-    curl_close($curl);
+curl_setopt_array($curl, [
+    CURLOPT_URL             => $get_payment_url,
+    CURLOPT_RETURNTRANSFER  => true,
+    CURLOPT_ENCODING        => "",
+    CURLOPT_MAXREDIRS       => 10,
+    CURLOPT_TIMEOUT         => 30,
+    CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST   => "GET",
+    CURLOPT_HTTPHEADER      => [
+        "Accept:            application/json",
+        "Content-Type:      application/json",
+        "x-api-version:     " . API_VERSION,
+        "x-client-id:       " . $app_id,
+        "x-client-secret:   " . $secret_key
+    ],
+]);
 
-    $cf_order = json_decode($response);
+$response = curl_exec($curl);
 
-    if ($cf_order === null) {
-        throw new Exception("Failed to decode JSON response");
-    }
-} catch (Exception $e) {
+curl_close($curl);
+
+$cf_order = json_decode($response);
+
+$result = mysql_fetch_assoc(select_query('tblinvoices', '*', array("id" => $invoice_id)));
+$invoice_amount = $result['total'];
+
+if (is_object($cf_order) && isset($cf_order->message)) {
     $success = false;
-    $error = "WHMCS_ERROR: " . $e->getMessage(); // Get the specific error message
-    \Sentry\init([
-        'dsn' => 'https://d8571a24c9b11025a1471d5e6aee55ce@o330525.ingest.sentry.io/4506552108187648',
-        'release' => CASHFREE_PLUGIN_VERSION . '@whmcs',
-        'environment' => $mode,
-    ]);
-    Sentry\captureException($e);
-    $cf_order = null;
-}
-
-if (null !== $cf_order && !empty($cf_order[0]->payment_status))
-{
+    $error = $cf_order->message;
+} elseif (is_array($cf_order) && isset($cf_order[0]) && isset($cf_order[0]->payment_status)) {
     if($cf_order[0]->payment_status == 'SUCCESS')
     {
         $transaction_id = $cf_order[0]->cf_payment_id;
-        $paymentAmount = $cf_order[0]->order_amount;
-        $success = true;
-    }
-    else {
+        $cf_order_amount = $cf_order[0]->order_amount;
+        if(round($invoice_amount, 2) == round($cf_order_amount, 2)) {
+            $success = true;
+        } else {
+            $error = 'Amount Mismatched';
+            $success = false;
+        }
+    } else {
         $success = false;
         $error = $cf_order[0]->payment_message;
     }
-
 } else {
-    if(!empty($cf_order->message)) {
-        $error = $cf_order->message;
-    } else {
-        $error = "Unable to process your order. Please contact support.";
-    }
+    // Handle other cases or errors
+    $error = "Unable to process your order. Please contact support.";
     $success = false;
-
 }
-
 
 //Check if payment successfully paid
 
@@ -133,26 +120,21 @@ if ($success === true)
     * @param string $transaction_id
     */
     # Apply Payment to Invoice: invoiceid, transaction_id, amount paid, fees, modulename
+    // var_dump(checkTransIdExist($transaction_id));
+    // die;
     if (!checkTransIdExist($transaction_id)){
-        $invoice_details = Capsule::table('tblinvoices')
-            ->where('id', $invoice_id)
-            ->first();
-
-        $amount = $invoice_details->total;
-        
-        addInvoicePayment($invoice_id, $transaction_id, $amount, 0, $gateway_params["name"]);
+        addInvoicePayment($invoice_id, $transaction_id, $invoice_amount, 0, $gateway_params["name"]);
         
         # Successful
         # Save to Gateway Log: name, data array, status
-        logTransaction($gateway_params["name"], $cf_order[0], "Successful");
+        logTransaction($gateway_params['name'], $cf_order[0], "Successful");
     }
-
     header("Location: ".$gateway_params['systemurl']."viewinvoice.php?id=" . $invoice_id."&paymentsuccess=true");
 }
 else 
 {
     # Save to Gateway Log: name, data array, status
-    logTransaction($gateway_params["name"], $_REQUEST["order_id"], "Unsuccessful-".$error . ". Please check cashfree dashboard for order id: ".$cashfree_order_id);
+    logTransaction($gateway_params['name'], $_REQUEST['order_id'], "Unsuccessful-".$error . ". Please check cashfree dashboard for order id: ".$cashfree_order_id);
 
     header("Location: ".$gateway_params['systemurl']."viewinvoice.php?id=" . $invoice_id."&paymentfailed=true");
 }
